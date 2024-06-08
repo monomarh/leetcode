3b4b52f55ee21ad4e6e297a8353a6a76dc910791
monomarh <r.monomarh@gmail.com>
2024-06-08 10:04:31 +0200

    2

--- 2_add_two_numbers/main.go
+++ 2_add_two_numbers/main.go
@@ -10,6 +10,15 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func main() {
+	var l1, l2 ListNode
+
+	l1 = *createList([]int{2, 4, 9})
+	l2 = *createList([]int{5, 6, 4, 9})
+
+	fmt.Println(addTwoNumbers(&l1, &l2))
+}
+
 func toNumber(l *ListNode) (num int) {
 	arr := []int{}
 	for i := l; i != nil; i = i.Next {
@@ -23,21 +32,44 @@ func toNumber(l *ListNode) (num int) {
 	return
 }
 
-func main() {
-	var l1, l2 ListNode
+func createList(arr []int) *ListNode {
+	var list *ListNode
 
-	l1 = *createList([]int{2, 4, 9})
-	l2 = *createList([]int{5, 6, 4, 9})
+	for pos, val := range arr {
+		if pos == 0 {
+			list = &ListNode{val, nil}
+		} else {
+			list = &ListNode{val, list}
+		}
+	}
 
-	fmt.Println(addTwoNumbers(&l1, &l2))
+	return list
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	var num1, num2 = toNumber(l1), toNumber(l2)
 
+	fmt.Println(num1, num2)
+
 	sum := toArray(num1 + num2)
 
 	result := createList(sum)
 
+	for i := result; i != nil; i = i.Next {
+		fmt.Println(i)
+	}
+
 	return result
+}
+
+func toArray(num int) (arr []int) {
+	if num == 0 {
+		return []int{0}
+	}
+
+	for num > 0 {
+		arr = append([]int{num % 10}, arr...)
+		num = num / 10
+	}
+	return
 }
